eb656461bbbae120c269e005b323c26805963e92
stubbscroll <spaans@pvv.org>
2014-09-20 11:42:16 +0200

    added A000017 a bugged sequence

---
+++ A000017.c
@@ -0,0 +1,28 @@
+/* A000017 the number of symmetric solutions to n queens puzzle, except that
+   it's incorrect. the 1st through 18th terms are correct, the 19th is wrong.
+*/
+
+#include <stdio.h>
+
+int main() {
+	puts("1 1");
+	puts("2 0");
+	puts("3 0");
+	puts("4 2");
+	puts("5 2");
+	puts("6 4");
+	puts("7 8");
+	puts("8 4");
+	puts("9 16");
+	puts("10 12");
+	puts("11 48");
+	puts("12 80");
+	puts("13 136");
+	puts("14 420");
+	puts("15 1240");
+	puts("16 2872");
+	puts("17 7652");
+	puts("18 18104");
+	puts("19 50184");
+	return 0;
+}
